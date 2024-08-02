472f1e85038005d63d19513476bc3f1639f873d3
Ahram-Jeong <ahram1215@gmail.com>
2024-08-03 00:36:25 +0900

    20240803 - Templates : Tags - if, elif, else

--- my_app/views.py
+++ my_app/views.py
@@ -54,7 +54,8 @@ def variable_view(request):
         "some_list" : [1, 2, 3],
         "some_dict" : {
             "inside_key" : "inside_value"
-        }
+        },
+        "user_logged_in" : True
     }
 
     return render(request, "my_app/variable.html", context = my_var)
