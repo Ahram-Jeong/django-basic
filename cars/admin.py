3b284eba460b517c915a302b9a5d202826c60b5f
Ahram-Jeong <ahram1215@gmail.com>
2024-08-06 15:46:58 +0900

    20240806 - Django Admin

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from cars.models import Car
 
 # Register your models here.
+# admin.site.register(Car)
+
+class CarAdmin(admin.ModelAdmin):
+    # 객체의 필드 순서 지정 가능
+    # fields = ["year", "brand"]
+
+    # 관리자 페이지의 필드셋 지정 가능
+    fieldsets = [
+        ("TIME INFORMATION", {"fields" : ["year"]}),
+        ("CAR INFORMATION", {"fields" : ["brand"]})
+    ]
+
+admin.site.register(Car, CarAdmin)
