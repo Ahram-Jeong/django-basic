9adb556f5026af750b400a19ad677097e6fe63a4
Ahram-Jeong <ahram1215@gmail.com>
2024-08-08 00:20:06 +0900

    20240808 - Django Forms : ModelForm 커스터마이징

--- cars2/forms.py
+++ cars2/forms.py
@@ -13,4 +13,18 @@ from django.forms import ModelForm
 class ReviewForm(ModelForm):
     class Meta: # 단일 속성이 있는 클래스
         model = Review
-        fields = ["first_name", "last_name", "stars"] # 상호작용 하고자 하는 모델의 필드 연결
+        # fields = ["first_name", "last_name", "stars"] # 상호작용 하고자 하는 모델의 필드 연결
+        fields = "__all__" # 모든 모델 필드를 폼 필드로 전달
+        # 레이블 지정
+        labels = {
+            "first_name" : "Your first name",
+            "last_name" : "Your last name",
+            "stars" : "Rating"
+        }
+        # 커스텀 Error msg
+        error_messages = {
+            "stars" : {
+                "min_value" : "최소 점수는 1점 입니다.",
+                "max_value" : "최대 점수는 5점 입니다."
+            }
+        }
