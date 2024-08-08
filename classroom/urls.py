6c329db842d2c7d599f2ce37a871a81f23750537
Ahram-Jeong <ahram1215@gmail.com>
2024-08-08 14:23:21 +0900

    20240808 - Django CBV : FormView

--- classroom/urls.py
+++ classroom/urls.py
@@ -8,5 +8,6 @@ app_name = "classroom"
 urlpatterns= [
     # path("", home_view, name = "home") # 함수 기반 뷰 연결 : path는 뷰를 함수라고 기대 함 (home_view)
     path("", HomeView.as_view(), name = "home"), # 클래스 기반 뷰 연결 : as_view() 메소드 호출 -> 클래스를 가지고 경로에 대한 함수를 반환
-    path("thank_you/", ThankYouView.as_view(), name = "thank_you")
+    path("thank_you/", ThankYouView.as_view(), name = "thank_you"),
+    path("contact/", ContactFormView.as_view(), name = "contact")
 ]
--- classroom/views.py
+++ classroom/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
+from django.urls import reverse_lazy
+from classroom.forms import ContactForm
 
 # Create your views here.
 # 함수 기반 뷰
@@ -12,3 +14,19 @@ class HomeView(TemplateView):
 
 class ThankYouView(TemplateView):
     template_name = "classroom/thank_you.html"
+
+class ContactFormView(FormView):
+    # 폼 클래스 연결
+    form_class = ContactForm # 인스턴스 생성이 아닌, 연결만 하는 것
+    template_name = "classroom/contact.html"
+
+    # 성공 시, 이동하는 url (not a template.html)
+    # 함수 기반 뷰에서 reverse(str 반환)를 사용했다면, 클래스 기반 뷰에서는 reverse_lazy(객체 반환)사용
+    success_url = reverse_lazy("classroom:thank_you")
+
+    # 유효한 form이 post 되었을 때 호출
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        
+        # 함수 기반 뷰의 ContactForm(request.POST)와 유사
+        return super().form_valid(form)
